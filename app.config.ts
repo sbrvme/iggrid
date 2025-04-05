06e61581257a4cb59a0fa21106c03d3b8b9bfd90
sbrvme <atyxyta@gmail.com>
2025-04-05 18:29:39 +0200

    int start

--- app.config.ts
+++ app.config.ts
@@ -1,3 +1,13 @@
 import { defineConfig } from "@solidjs/start/config";
 
-export default defineConfig({});
+export default defineConfig({
+server: {
+    baseURL: process.env.BASE_PATH,
+    static: true,
+    prerender: {
+      failOnError: true,
+      routes: ["/"],
+      crawlLinks: true,
+    },
+  },
+});
